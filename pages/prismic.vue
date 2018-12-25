1a52949d45ee304a4da689ec6920485d449b1f7f
admin <admin@almogtama3.com>
2018-12-25 12:14:37 +0300

    add prismic-nuxt

---
+++ pages/prismic.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <div v-html="$prismic.dom.RichText.asHtml(document.data.title, $prismic.linkResolver)"></div>
+  </section>
+</template>
+ 
+<script>
+export default {
+  async asyncData({ app, error }) {
+    let document = await app.$prismic.api.getByUID('page', 'my-page')
+
+    if (document) {
+      return { document }
+    } else {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
+}
+</script>
+ 
+<style scoped>
+.title {
+  margin: 50px 0;
+}
+</style>
