590beb1ff6a7a107bc41735c4dbfdec29fa9385e
admin <admin@almogtama3.com>
2018-12-26 08:44:33 +0300

    26-12-2018

--- pages/prismic.vue
+++ pages/prismic.vue
@@ -1,23 +1,33 @@
 <template>
-  <section>
-    <div v-html="$prismic.dom.RichText.asHtml(document.data.title, $prismic.linkResolver)"></div>
-  </section>
+  <article>
+    <h1 class="article-title" v-html="$prismic.dom.RichText.asHtml(document.data.title, $prismic.linkResolver)"></h1>
+    <div class="cover">
+      <img :src="document.data.cover_image.url" :alt="document.data.cover_image.alt"  />
+      <!--<img :src="$prismic.dom.Link.url(document.data.cover_image.url, $prismic.linkResolver)" :alt="document.data.cover_image.alt"  />-->
+    </div>
+      <div class="article-content" v-html="$prismic.dom.RichText.asHtml(document.data.content, $prismic.linkResolver)"></div>
+
+<div id="test">
+  <b>test:</b> {{document}}
+</div>
+  </article>
 </template>
- 
+
 <script>
 export default {
   async asyncData({ app, error }) {
-    let document = await app.$prismic.api.getByUID('page', 'my-page')
+    let document = await app.$prismic.api.getByUID('article', 'article-1')
+    console.log('document:', document)
 
-    if (document) {
-      return { document }
+    if (document && !document.error) {
+      return { document } //  return document
     } else {
       error({ statusCode: 404, message: 'Page not found' })
     }
   }
 }
 </script>
- 
+
 <style scoped>
 .title {
   margin: 50px 0;
